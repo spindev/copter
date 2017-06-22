ba2bdab04fc95ba97107585a16a85e1bcf77ed63
spindev <tillspi@web.de>
2017-06-22 23:20:26 +0200

    first version

--- src/copter.py
+++ src/copter.py
@@ -1,15 +1,64 @@
-import time
-import lib.BMP085 as BMP085
+import time 
+import lib.xbox as xbox 
+import pigpio 
+import RPi.GPIO as GPIO
 
-sensor = BMP085.BMP085()
+MIN = 1000
+MAX = 2500
+
+SPEED = 1000
+
+STEP = 100
+SLEEP_TIME = 0.5
+
+esc1 = 6
+esc2 = 13
+esc3 = 19
+esc4 = 26 
+
+def setSpeed(value):
+	pi.set_servo_pulsewidth(esc1, value)
+    	pi.set_servo_pulsewidth(esc2, value)
+	pi.set_servo_pulsewidth(esc3, value)
+	pi.set_servo_pulsewidth(esc4, value)
+
+def calibrate():
+    setSpeed(MAX)
+    time.sleep(2)
+    setSpeed(MIN)
+    time.sleep(2)
+
+    return
+
+# Main
+
+GPIO.setmode(GPIO.BCM)
+pi = pigpio.pi()
+joy = xbox.Joystick()
+
+print("Calibrate...")
+
+calibrate()
+
+print("Lets FLY...") 
 
 try:
-	while True:
-		print "Temp" + str(sensor.readTemperature())
-
-		time.sleep(1)
-
+	while not joy.Back():
+		if joy.dpadUp() and  SPEED < MAX:
+			SPEED = SPEED + STEP
+		
+		if joy.dpadDown() and SPEED > MIN:
+			SPEED = SPEED - STEP
+	
+		setSpeed(SPEED)
+		print "Speed: " + str(SPEED)
+		time.sleep(SLEEP_TIME)	
+	
 except KeyboardInterrupt:
-	print "End.."
+	print "End..." 
 
-
+finally:
+    	setSpeed(0)
+    	pi.stop()
+	joy.close()
+	
