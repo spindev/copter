eae1c92bcf0ad763f2de1e17704361ec64b4b595
spindev <tillspi@web.de>
2017-06-29 22:01:47 +0200

    XboxController

--- src/copter.py
+++ src/copter.py
@@ -1,7 +1,8 @@
 import time 
-import lib.xbox as xbox 
 import pigpio 
 import RPi.GPIO as GPIO
+import os
+import lib.XboxController as XboxController
 
 MIN = 1000
 MAX = 2500
@@ -17,48 +18,71 @@ esc3 = 19
 esc4 = 26 
 
 def setSpeed(value):
-	pi.set_servo_pulsewidth(esc1, value)
-    	pi.set_servo_pulsewidth(esc2, value)
-	pi.set_servo_pulsewidth(esc3, value)
-	pi.set_servo_pulsewidth(esc4, value)
+    pi.set_servo_pulsewidth(esc1, value)
+    pi.set_servo_pulsewidth(esc2, value)
+    pi.set_servo_pulsewidth(esc3, value)
+    pi.set_servo_pulsewidth(esc4, value)
+    print("speed: ") + str(value)
 
 def calibrate():
+    print("Calibrate...")
     setSpeed(MAX)
     time.sleep(2)
     setSpeed(MIN)
     time.sleep(2)
+    print("lets fly...")
 
     return
+
+def finish():
+    setSpeed(0)
+    pi.stop()
+    xboxCont.stop()
+    os._exit(0)
+
+def startCallBack(value):
+    if value == 0:
+        return
+
+    calibrate()
+
+def dpadCallBack(value):
+    global SPEED
+
+    if(value[0] == 1 and SPEED < MAX):
+        SPEED = SPEED + STEP
+        setSpeed(SPEED)
+
+    if(value[0] == -1 and SPEED > MIN):
+        SPEED = SPEED - STEP
+        setSpeed(SPEED)
 
 # Main
 
 GPIO.setmode(GPIO.BCM)
 pi = pigpio.pi()
-joy = xbox.Joystick()
 
-print("Calibrate...")
+xboxCont = XboxController.XboxController(
+    controllerCallBack = None,
+    joystickNo = 0,
+    deadzone = 0.1,
+    scale = 1,
+    invertYAxis = True)
 
-calibrate()
+xboxCont.setupControlCallback(xboxCont.XboxControls.START, startCallBack)
+xboxCont.setupControlCallback(xboxCont.XboxControls.BACK, finish)
+xboxCont.setupControlCallback(xboxCont.XboxControls.DPAD, dpadCallBack)
 
-print("Lets FLY...") 
+xboxCont.start()
+
+print("init successfull...")
 
 try:
-	while not joy.Back():
-		if joy.dpadUp() and  SPEED < MAX:
-			SPEED = SPEED + STEP
-		
-		if joy.dpadDown() and SPEED > MIN:
-			SPEED = SPEED - STEP
-	
-		setSpeed(SPEED)
-		print "Speed: " + str(SPEED)
-		time.sleep(SLEEP_TIME)	
+	while True:
+		time.sleep(SLEEP_TIME)
 	
 except KeyboardInterrupt:
-	print "End..." 
+	print("end...")
 
 finally:
-    	setSpeed(0)
-    	pi.stop()
-	joy.close()
-	
+    finish()
